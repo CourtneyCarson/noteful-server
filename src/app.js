5c83ef2cb89c3f0ed2cc056eab44c57d7c1c4d73
Courtney Carson <cmacarson@gmail.com>
2020-11-05 16:06:34 -0800

    app, config, server

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const {NODE_ENV} = require('./config')
+const { NODE_ENV } = require('./config')
+const notesRouter = require("../router/notes-router.js");
+const foldersRouter = require("../router/folders-router");
+
+
 
 const app = express()
 
@@ -14,6 +18,11 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+
+app.use("/api/notes", notesRouter);
+app.use("/api/folders", foldersRouter);
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')

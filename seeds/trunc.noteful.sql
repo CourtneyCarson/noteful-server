e98f0f6a2dd2bb0d1372f7046c5c30670b7994a1
Courtney Carson <cmacarson@gmail.com>
2020-11-16 17:47:34 -0800

    updates

--- seeds/trunc.noteful.sql
+++ seeds/trunc.noteful.sql
@@ -1,13 +1,3 @@
-INSERT INTO folders (folder_name)
-VALUES
-('Made Up Words'), 
-('Folder Names'), 
-('Name of Folder'); 
-
-INSERT INTO notes (id, note_name, content, modified)
-VALUES 
-(1, 'dis is a note', 'lorem ipsum, yadda yadda', now() ),
-(2,'dis is a note', 'lorem ipsum, words, words', now() ),
-(3,'dis is a note', 'lorem ipsum, some more words here', now());
-
-
+TRUNCATE TABLE 
+folders,
+notes;
